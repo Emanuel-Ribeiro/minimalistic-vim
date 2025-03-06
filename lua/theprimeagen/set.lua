f4d901c84f685b095c6775206a5613e373ec51e6
Emanuel Ribeiro <lima.ribeiro.emanuel@gmail.com>
2025-03-06 15:07:37 -0300

    improving netrw

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -13,3 +13,13 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+-- Netrw enhancements
+vim.g.netrw_banner = 0                -- Hide the banner
+vim.g.netrw_liststyle = 1             -- Default to detailed view (0=thin, 1=long, 2=wide, 3=tree)
+vim.g.netrw_browse_split = 0          -- Open files in the same window
+vim.g.netrw_winsize = 25              -- Set width to 25% of screen
+vim.g.netrw_altv = 1                  -- Open splits to the right
+vim.g.netrw_sizestyle = "H"           -- Human-readable file sizes
+vim.g.netrw_list_hide = '\\.git$'     -- Hide .git directories
+vim.g.netrw_bufsettings = 'noma nomod nu rnu nowrap ro nobl' -- Enable line numbers in netrw
+
