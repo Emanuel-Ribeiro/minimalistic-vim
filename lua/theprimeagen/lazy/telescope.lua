9a655e2375ffac9c9f6f44dd17e231518ca1b22d
Emanuel Ribeiro <lima.ribeiro.emanuel@gmail.com>
2025-03-21 14:53:48 -0300

    fixing grep stuff

--- lua/theprimeagen/lazy/telescope.lua
+++ lua/theprimeagen/lazy/telescope.lua
@@ -1,41 +1,67 @@
 return {
-    "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+  tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
 
-    config = function()
-        require('telescope').setup({
-          defaults = {
-            layout_strategy = "vertical",
-            layout_config = {
-              vertical = {
-                -- Adjust these settings as desired:
-                preview_height = 0.5,  -- controls the height of the preview window
-                mirror = false,        -- if true, swaps the positions of the results and preview windows
-              },
-            },
+  config = function()
+    require('telescope').setup({
+      defaults = {
+        layout_strategy = "vertical",
+        layout_config = {
+          vertical = {
+            preview_height = 0.7,  -- Ajusta a altura do preview
+            mirror = false,        -- Mantém a posição padrão do preview
           },
-        })
+          preview_cutoff = 1, -- Garante que o preview nunca seja escondido em telas pequenas
+        },
+      },
+    })
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-    end
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+    vim.keymap.set('n', '<leader>pws', function()
+      local word = vim.fn.expand("<cword>")
+      builtin.grep_string({
+        search = word,
+        layout_strategy = "vertical",
+        layout_config = { preview_cutoff = 1, preview_height = 0.4 },
+        previewer = true
+      })
+    end)
+
+    vim.keymap.set('n', '<leader>pWs', function()
+      local word = vim.fn.expand("<cWORD>")
+      builtin.grep_string({
+        search = word,
+        layout_strategy = "vertical",
+        layout_config = { preview_cutoff = 1, preview_height = 0.4 },
+        previewer = true
+      })
+    end)
+
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.live_grep({
+        layout_strategy = "vertical",
+        layout_config = { preview_cutoff = 1, preview_height = 0.4 },
+        previewer = true
+      })
+    end)
+
+    vim.keymap.set('n', '<leader>lg', function()
+      builtin.live_grep({
+        layout_strategy = "vertical",
+        layout_config = { preview_cutoff = 1, preview_height = 0.4 },
+        previewer = true
+      })
+    end)
+
+    vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+  end
 }
 
